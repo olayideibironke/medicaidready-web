6cdf097318068c1a54880dc78bc3c377880bb336
Olayide Ibironke <ibironkeolayide@gmail.com>
2026-05-11 13:18:32 -0400

    Add global favicon document

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+﻿import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta name="theme-color" content="#0f766e" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
